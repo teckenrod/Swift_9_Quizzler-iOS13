7bcefc2afefa9c8dfdc7047cbf6ab1b9f8cf5fae
teckenrod <treyeck2@gmail.com>
2023-01-30 19:22:03 -0600

    bounds checking

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var falseButton: UIButton!
     
-    let quiz = ["Four + Two is equal to Six",
-                "Five - Three is grester than One",
-                "Three + Eight is less than Ten"]
+    let quiz = [["Four + Two is equal to Six", "True"],
+                ["Five - Three is grester than One", "True"],
+                ["Three + Eight is less than Ten", "False"]]
     
     var questionNumber = 0
     
@@ -28,12 +28,24 @@ class ViewController: UIViewController {
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        questionNumber += 1
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        if userAnswer == actualAnswer {
+            
+        }
+        
+        if questionNumber < quiz.count - 1 {
+            questionNumber += 1
+        }
+        else {
+            questionNumber = 0
+        }
+        
         updateUI()
     }
     
     func updateUI() {
-        questionLabel.text = quiz[questionNumber]
+        questionLabel.text = quiz[questionNumber][0]
     }
 }
 
